84e3deeb8e6ae7b2458ee6419d96ca976f14c026
Tom Leese <leese.thomas81@gmail.com>
2013-11-22 15:41:07 +0000

    Add stub event handlers to robot

--- src/Bot.java
+++ src/Bot.java
@@ -1,6 +1,7 @@
 package bot;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import robocode.*;
 
 public class Bot extends RateControlRobot {
@@ -25,8 +26,74 @@ public class Bot extends RateControlRobot {
     }
 
     @Override
+    public void onBattleEnded(BattleEndedEvent e) {
+        System.out.println("The battle is over: " + e);
+    }
+
+    @Override
+    public void onBulletHit(BulletHitEvent e) {
+        System.out.println("Out bullet hit a robot: " + e);
+    }
+
+    @Override
+    public void onBulletHitBullet(BulletHitBulletEvent e) {
+        System.out.println("Our bullet hit another: " + e);
+    }
+
+    @Override
+    public void onBulletMissed(BulletMissedEvent e) {
+        System.out.println("Our bullet missed: " + e);
+    }
+
+    @Override
+    public void onDeath(DeathEvent e) {
+        System.out.println("We've died: " + e);
+    }
+
+    @Override
+    public void onHitByBullet(HitByBulletEvent e) {
+        System.out.println("We've been hit: " + e);
+    }
+
+    @Override
+    public void onHitRobot(HitRobotEvent e) {
+        System.out.println("We've crashed into a robot: " + e);
+    }
+
+    @Override
+    public void onHitWall(HitWallEvent e) {
+        System.out.println("We've crashed into a wall: " + e);
+    }
+
+    @Override
+    public void onPaint(Graphics2D g) {
+        super.onPaint(g);
+        // perhaps we should do some debug drawing ehre
+    }
+
+    @Override
+    public void onRobotDeath(RobotDeathEvent e) {
+        System.out.println("Someone else has died: " + e);
+    }
+
+    @Override
+    public void onRoundEnded(RoundEndedEvent e) {
+        System.out.println("The round is over: " + e);
+    }
+
+    @Override
     public void onScannedRobot(ScannedRobotEvent e) {
         System.out.println("Seen a robot: " + e);
+    }
+
+    @Override
+    public void onStatus(StatusEvent e) {
+        System.out.println("We've got some status information: " + e);
+    }
+
+    @Override
+    public void onWin(WinEvent e) {
+        System.out.println("We won! " + e);
     }
 
 }
