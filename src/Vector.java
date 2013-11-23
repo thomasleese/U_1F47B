0d87b68cf985e9e0d9c625425254838142c4aa70
Yan Zhang <cyanogenoid@cyanogenoid.com>
2013-11-23 16:13:17 +0000

    Add Vector class

---
+++ src/Vector.java
@@ -0,0 +1,21 @@
+package bot;
+
+public class Vector extends java.awt.geom.Point2D.Double {
+
+	public Vector(double x, double y) {
+		super(x, y);
+	}
+
+	public double length() {
+		return Math.sqrt(this.lengthSq());
+	}
+
+	public double lengthSq() {
+		return this.x*this.x + this.y*this.y;
+	}
+
+	public Vector add(Vector v, double coefficient)
+	{
+		return new Vector(this.getX() + coefficient * v.getX(), this.getY() + coefficient * v.getY());
+	}
+}
