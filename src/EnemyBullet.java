a870a44b640b72b8728f905570b4581a07fe765b
Yan Zhang <cyanogenoid@cyanogenoid.com>
2013-11-23 16:13:42 +0000

    Add EnemyBullet class
    This tracks the position, speed and power of the bullet at any given
    time.

---
+++ src/EnemyBullet.java
@@ -0,0 +1,33 @@
+package bot;
+
+public class EnemyBullet {
+    
+    private Vector a;
+    private Vector b;
+    private double power;
+    private long time;
+
+    public EnemyBullet(double positionX, double positionY, double power, double angleD, long time) {
+        double angleR = Math.toRadians(angleD);
+        this.a = new Vector(positionX, positionY);
+        this.b = new Vector(Math.sin(angleR) * this.getSpeed(), Math.cos(angleR) * this.getSpeed());
+        this.power = power;
+        this.time = time;
+    }
+
+    public Vector getPosition(long time) {
+        return a.add(b, time - this.time);
+    }
+
+    public double getPower() {
+        return this.power;
+    }
+
+    public double getSpeed() {
+        return 20 - (3 * this.getPower());
+    }
+
+    public long getTime() {
+        return this.time;
+    }
+}
