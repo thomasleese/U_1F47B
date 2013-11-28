29de90bce447ffcd3013481658e3ade4e3b92ee7
Tom Leese <leese.thomas81@gmail.com>
2013-11-28 21:20:09 +0000

    Make SimpleGun turn to face a robot if we collide into one

--- src/SimpleGun.java
+++ src/SimpleGun.java
@@ -7,6 +7,9 @@ public class SimpleGun extends Gun {
 
     private double coefficient;
 
+    private boolean overrideRotation = false;
+    private double overrideRotationValue;
+
     public SimpleGun(State state, double coefficient) {
         super(state);
         this.coefficient = coefficient;
@@ -14,7 +17,13 @@ public class SimpleGun extends Gun {
 
     @Override
     public void execute() {
-        if(this.state.latestRobot != null) {
+        if (this.overrideRotation) {
+            this.rotation = this.overrideRotationValue;
+            this.overrideRotation = false;
+            return;
+        }
+
+        if (this.state.latestRobot != null) {
             OtherRobot.Tick tick = this.state.latestRobot.getHistory(-1);
             double rotation = tick.bearing + this.state.owner.getHeading() - // absolute rotation to enemy
                               this.state.owner.getGunHeading(); // relative rotation to gun
@@ -22,6 +31,14 @@ public class SimpleGun extends Gun {
         } else {
             this.rotation = Double.POSITIVE_INFINITY;
         }
+    }
+
+    @Override
+    public void onHitRobot(HitRobotEvent e) {
+        double rotation = e.getBearing() + this.state.owner.getHeading() - // absolute rotation to enemy
+                          this.state.owner.getRadarHeading();   // relative rotation to gun
+        this.overrideRotationValue = this.coefficient * Utils.normalRelativeAngleDegrees(rotation); // normalise
+        this.overrideRotation = true;
     }
 
 }
