8fcc18a6f3e9c618054f2fd1ed67b7c571b006a4
Yan Zhang <cyanogenoid@cyanogenoid.com>
2013-11-29 22:14:15 +0000

    Let PredictiveGun draw predicted enemy position

--- src/PredictiveGun.java
+++ src/PredictiveGun.java
@@ -1,11 +1,14 @@
 package bot;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import robocode.*;
 import robocode.util.*;
 
 public class PredictiveGun extends Gun {
 
     private double coefficient;
+    private Vector predVec;
 
     public PredictiveGun(State state, double coefficient) {
         super(state);
@@ -36,14 +39,14 @@ public class PredictiveGun extends Gun {
 
             double timeSteps = dist / projectileSpeed;
 
-            Vector predVec = this.state.trackingRobot.predictLocation((int)timeSteps + 1, OtherRobot.TurnBehaviours.keepTurn, OtherRobot.SpeedBehaviours.keepSpeed);
+            this.predVec = this.state.trackingRobot.predictLocation((int)timeSteps + 1, OtherRobot.TurnBehaviours.keepTurn, OtherRobot.SpeedBehaviours.keepSpeed);
 
             double afterDist = Util.getDistance(locX - predVec.getX(), locY - predVec.getY());
             double afterTimeSteps = afterDist / projectileSpeed;
 
             double adqTimeSteps = (afterTimeSteps * 0.8 + timeSteps * 0.2); // take weighted average
 
-            predVec = this.state.trackingRobot.predictLocation((int)adqTimeSteps + 1, OtherRobot.TurnBehaviours.keepTurn, OtherRobot.SpeedBehaviours.keepSpeed);
+            this.predVec = this.state.trackingRobot.predictLocation((int)adqTimeSteps + 1, OtherRobot.TurnBehaviours.keepTurn, OtherRobot.SpeedBehaviours.keepSpeed);
 
             double litDir = Util.getAngle(locX - predVec.getX(), locY - predVec.getY());
 
@@ -52,6 +55,17 @@ public class PredictiveGun extends Gun {
         } else {
             // leave the rotation as it is allready
         }
+    }
+
+    @Override
+    public void onPaint(Graphics2D g) {
+        double x = this.predVec.getX();
+        double y = this.predVec.getY();
+
+        int a = 36;
+
+        g.setColor(new Color(255, 255, 0));
+        g.drawRect((int) x - a/2, (int) y - a/2, a, a);
     }
 
 }
